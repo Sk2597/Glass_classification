efcc492aa3616bed6dfe6f063db3fba67aa12d1a
sk2597 <smrutikarade@MacBook-Air-901.local>
2020-08-23 01:29:47 +0530

    updated settings

--- classification/settings.py
+++ classification/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2igt#*4-xh9h5q41a2)h-p=bjmn!ctyms1&!6*hwk5)dhc70vq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['glass-classification.herokuapp.com','127.0.0.1:8000']
 
 
 # Application definition
